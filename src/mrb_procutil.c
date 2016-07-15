14b3a7bc66bc290af1f2fec339e73eb5cd5ace0c
Uchio KONDO <udzura@pepabo.com>
2016-07-15 12:00:54 +0900

    Revert "Freed"
    This reverts commit 9b48795832b329c8521e679646d3f51d74abf26e.

--- src/mrb_procutil.c
+++ src/mrb_procutil.c
@@ -12,7 +12,6 @@
 #include "mruby/string.h"
 #include "mrb_procutil.h"
 
-#include <stdlib.h>
 #include <unistd.h>
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
@@ -40,7 +39,6 @@ static mrb_value mrb_procutil_daemon_fd_reopen(mrb_state *mrb, mrb_value self)
   TRY_REOPEN(fp, "/dev/null", "r", stdin);
   TRY_REOPEN(fp, "/dev/null", "w", stdout);
   TRY_REOPEN(fp, "/dev/null", "w", stderr);
-  free(fp);
 
   return mrb_true_value();
 }
